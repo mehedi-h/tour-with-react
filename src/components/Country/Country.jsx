8afe1ef2edd86893a75aa2e5854875d5a554daca
mehedi-h <mehedihs2015@gmail.com>
2023-09-10 17:21:24 +0600

    modified code

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
 import "./Country.css"
 
 const Country = ({country}) => {
     console.log(country)
-    const {name, flags, capital, population, area, maps, currencies, languages} = country
+    const {name, flags, capital, population, area, maps, cioc, cca3, currencies, languages} = country
+    const [visited, setVisited] = useState(false)
+    const handleVisited = () => {
+        setVisited(!visited)
+    }
     return (
         <div className='country'>
             <div className="img">
@@ -11,12 +16,14 @@ const Country = ({country}) => {
             </div>
             <h4>Informations</h4>
             <div className="info">
+                <p>Code : {cioc}, {cca3}</p>
                 <p>Capital : {capital}</p>
                 <p>Populations : {population}</p>
                 <p>Areas : {area}</p>
                 {/* <p>Currency : {currencies}</p> */}
                 {/* <p>Language : {languages}</p> */}
-                <a href={maps.googleMaps}>On Maps</a>
+                {/* <a href={maps.googleMaps}>On Maps</a> */}
+                <button onClick={handleVisited}>{visited ? 'Visited' : 'Going...'}</button>
             </div>
         </div>
     );
