d191ace51cda28a1f1cf1c1ae70a8c8d6d4f5db6
mehedi-h <mehedihs2015@gmail.com>
2023-09-10 21:21:04 +0600

    modified

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -4,18 +4,31 @@ import "./Countries.css"
 
 const Countries = () => {
     const [countries, setCountries] = useState([])
+    const [visitedCountries, setVisitedCountries] = useState([])
+
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
         .then (res => res.json())
         .then (data => setCountries(data))
     }, [])
+
+    const handleVisitedCountries = country => {
+            console.log('nepal visited')
+        console.log(country)
+    }
     
     return (
         <div>
             <h2>Countries : {countries.length}</h2>
+            <div>
+                <h4>Visited Country :</h4>
+                <ul>
+
+                </ul>
+            </div>
             <div className="country-container">
             {
-                countries.map(country => <Country key={country.cca3} country={country}></Country>)
+                countries.map(country => <Country key={country.cca3} visitHandlers={handleVisitedCountries} country={country}></Country>)
             }
             </div>
         </div>
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
 import "./Country.css"
 
-const Country = ({country}) => {
+const Country = ({country, visitHandlers}) => {
     console.log(country)
+    console.log(visitHandlers)
     const {name, flags, capital, population, area, maps, cioc, cca3, currencies, languages} = country
     const [visited, setVisited] = useState(false)
     const handleVisited = () => {
         setVisited(!visited)
     }
     return (
-        <div className='country'>
+        <div className={`country ${visited && 'visited'}`}>
             <div className="img">
                 <img src={flags.png} alt="National Flag"/>
                 <h3 className="name">{name?.common}</h3>
@@ -24,6 +25,8 @@ const Country = ({country}) => {
                 {/* <p>Language : {languages}</p> */}
                 {/* <a href={maps.googleMaps}>On Maps</a> */}
                 <button onClick={handleVisited}>{visited ? 'Visited' : 'Going...'}</button>
+                <br />
+                <button style={{margin: '10px 0'}}>Mark Visited</button>
             </div>
         </div>
     );
